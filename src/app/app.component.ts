c67a19b64f3fef9c921df7dbe5e738a48ee0e169
kirangsa <kiran.gsa@gmail.com>
2021-09-24 10:35:59 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name = 'I am kiran';
 
+  // Create an interface for user
   user = {
     firstName: 'kiran',
     lastName: 'Gopal',
     gender: 'Male',
-    dob: 'Wed Sep 08 2021 12:52:55 GMT+0530 (India Standard Time)'
+    dob: 'Wed Sep 08 2021 12:52:55 GMT+0530 (India Standard Time)',
   };
 }
