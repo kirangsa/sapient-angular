a6aa135c5109a90f1abf28ee40281d61571a942e
kirangsa <kiran.gsa@gmail.com>
2021-12-02 19:10:20 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ export class AppComponent {
   name;
   user = [
     {
-      firstName: 'Nissankala',
-      lastName: 'Ketya',
+      firstName: 'Nitin',
+      lastName: 'Singh',
       gender: 'Male',
       dob: 'Wed Sep 08 2021 12:52:55 GMT+0530 (India Standard Time)',
     },
